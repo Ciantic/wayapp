284f5d428ee94f7a05f1668f23d2012b87d6a8f8
Jari O. O. Pennanen <ciantic@oksidi.com>
2026-01-15 01:24:55 +0200

    move wgpu renderer back to its own file

--- src/egui_new/egui_wgpu_renderer.rs
+++ src/egui_new/egui_wgpu_renderer.rs
@@ -1,87 +1,104 @@
-#![allow(unused_imports)]
+// Original for Winit:
+// https://github.com/kaphula/winit-egui-wgpu-template/blob/master/src/egui_tools.rs
+//
+// MIT License
+// Copyright (c) 2024 Valtteri Vallius
 
-mod egui_view_manager;
-mod egui_wgpu_renderer;
-use egui::PlatformOutput;
-use egui::RawInput;
-pub use egui_view_manager::*;
+use egui::Context;
 use egui_wgpu::Renderer;
 use egui_wgpu::RendererOptions;
 use egui_wgpu::ScreenDescriptor;
-pub use egui_wgpu_renderer::*;
+use egui_wgpu::wgpu;
+use egui_wgpu::wgpu::CommandEncoder;
+use egui_wgpu::wgpu::Device;
+use egui_wgpu::wgpu::Queue;
+use egui_wgpu::wgpu::StoreOp;
+use egui_wgpu::wgpu::TextureFormat;
+use egui_wgpu::wgpu::TextureView;
 
-/// EGUI WGPU Renderer wrapper
-struct EguiWgpuRenderer {
-    context: egui::Context,
+pub struct EguiWgpuRenderer {
+    context: Context,
     renderer: Renderer,
     frame_started: bool,
 }
 
 impl EguiWgpuRenderer {
-    fn new(device: &wgpu::Device, output_format: wgpu::TextureFormat, msaa_samples: u32) -> Self {
-        let egui_context = egui::Context::default();
+    pub fn context(&self) -> &Context {
+        &self.context
+    }
+
+    pub fn context_mut(&mut self) -> &mut Context {
+        &mut self.context
+    }
+
+    pub fn new(
+        device: &Device,
+        output_color_format: TextureFormat,
+        output_depth_format: Option<TextureFormat>,
+        msaa_samples: u32,
+    ) -> EguiWgpuRenderer {
+        let egui_context = Context::default();
+
         let egui_renderer = Renderer::new(
             device,
-            output_format,
+            output_color_format,
             RendererOptions {
                 msaa_samples,
-                depth_stencil_format: None,
+                depth_stencil_format: output_depth_format,
+
                 ..Default::default()
             },
         );
 
-        Self {
+        EguiWgpuRenderer {
             context: egui_context,
             renderer: egui_renderer,
             frame_started: false,
         }
     }
 
-    fn context(&self) -> &egui::Context {
-        &self.context
+    pub fn ppp(&mut self, v: f32) {
+        self.context.set_pixels_per_point(v);
     }
 
-    fn begin_frame(&mut self, raw_input: RawInput) {
+    pub fn begin_frame(&mut self, raw_input: egui::RawInput) {
         self.context.begin_pass(raw_input);
         self.frame_started = true;
     }
 
-    fn end_frame_and_draw(
+    pub fn end_frame_and_draw(
         &mut self,
-        device: &wgpu::Device,
-        queue: &wgpu::Queue,
-        encoder: &mut wgpu::CommandEncoder,
-        window_surface_view: &wgpu::TextureView,
+        device: &Device,
+        queue: &Queue,
+        encoder: &mut CommandEncoder,
+        window_surface_view: &TextureView,
         screen_descriptor: ScreenDescriptor,
-    ) -> PlatformOutput {
+    ) -> egui::PlatformOutput {
         if !self.frame_started {
-            panic!("begin_frame must be called before end_frame_and_draw!");
+            panic!("begin_frame must be called before end_frame_and_draw can be called!");
         }
 
-        self.context
-            .set_pixels_per_point(screen_descriptor.pixels_per_point);
+        self.ppp(screen_descriptor.pixels_per_point);
+
         let full_output = self.context.end_pass();
 
         let tris = self
             .context
             .tessellate(full_output.shapes, self.context.pixels_per_point());
-
         for (id, image_delta) in &full_output.textures_delta.set {
             self.renderer
                 .update_texture(device, queue, *id, image_delta);
         }
-
         self.renderer
             .update_buffers(device, queue, encoder, &tris, &screen_descriptor);
-
         let rpass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
             color_attachments: &[Some(wgpu::RenderPassColorAttachment {
                 view: window_surface_view,
                 resolve_target: None,
                 depth_slice: None,
-                ops: wgpu::Operations {
-                    load: wgpu::LoadOp::Load,
-                    store: wgpu::StoreOp::Store,
+                ops: egui_wgpu::wgpu::Operations {
+                    load: egui_wgpu::wgpu::LoadOp::Load,
+                    store: StoreOp::Store,
                 },
             })],
             depth_stencil_attachment: None,
@@ -92,12 +109,12 @@ impl EguiWgpuRenderer {
 
         self.renderer
             .render(&mut rpass.forget_lifetime(), &tris, &screen_descriptor);
-
         for x in &full_output.textures_delta.free {
-            self.renderer.free_texture(x);
+            self.renderer.free_texture(x)
         }
 
         self.frame_started = false;
+
         full_output.platform_output
     }
 }
