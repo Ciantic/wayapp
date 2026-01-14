3202aeea0f1cf51c8c96b6b5abd90048620dbc7c
Jari O. O. Pennanen <ciantic@oksidi.com>
2026-01-15 01:30:40 +0200

    Move input handler back to its own file

--- src/egui_new/mod.rs
+++ src/egui_new/mod.rs
@@ -1,9 +1,11 @@
 #![allow(unused_imports)]
 
+mod egui_input_handler;
 mod egui_view_manager;
 mod egui_wgpu_renderer;
 use egui::PlatformOutput;
 use egui::RawInput;
+pub use egui_input_handler::*;
 pub use egui_view_manager::*;
 use egui_wgpu::Renderer;
 use egui_wgpu::RendererOptions;
