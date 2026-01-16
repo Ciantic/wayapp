f7bc93859a931beac875ec8d715d985b9da42f84
Jari O. O. Pennanen <ciantic@oksidi.com>
2026-01-16 15:25:32 +0200

    Rename

--- src/egui_new/mod.rs
+++ src/egui_new/mod.rs
@@ -1,12 +1,12 @@
 #![allow(unused_imports)]
 
 mod egui_input_handler;
-mod egui_view_manager;
+mod egui_surface_state;
 mod egui_wgpu_renderer;
 use egui::PlatformOutput;
 use egui::RawInput;
 pub use egui_input_handler::*;
-pub use egui_view_manager::*;
+pub use egui_surface_state::*;
 use egui_wgpu::Renderer;
 use egui_wgpu::RendererOptions;
 use egui_wgpu::ScreenDescriptor;
