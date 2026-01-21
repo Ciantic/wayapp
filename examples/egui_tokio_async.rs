6c57bd60db3642723739faa16fea7980b62d8996
Jari O. O. Pennanen <ciantic@oksidi.com>
2026-01-21 16:15:05 +0200

    Rename again

--- examples/egui_tokio_async.rs
+++ examples/egui_tokio_async.rs
@@ -103,7 +103,7 @@ async fn main() {
 
     spawn_ticking_thread(tx.clone());
 
-    let mut dispatcher = app.run_reader(move || tx.send(AppEvent::WaylandDispatch).unwrap());
+    let mut dispatcher = app.run_dispatcher(move || tx.send(AppEvent::WaylandDispatch).unwrap());
     loop {
         if let Some(event) = rx.recv().await {
             match event {
