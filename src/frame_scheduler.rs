bcc75af6298fcc482a07c229fbacaaa5f04edfef
Jari O. O. Pennanen <ciantic@oksidi.com>
2026-04-09 22:54:36 +0300

    Remove the old unusued scheduler (it was reinvent the wheel)

--- src/frame_scheduler.rs
+++ src/frame_scheduler.rs
@@ -7,16 +7,20 @@
 
 // Note: Try not to edit this with LLMs, locking logic needs to be verified
 // manually.
-#![allow(dead_code)]
+
 use std::sync::Arc;
-use std::sync::Condvar;
 use std::sync::Mutex;
+use std::sync::mpsc::RecvTimeoutError;
+use std::sync::mpsc::Sender;
+use std::sync::mpsc::channel;
 use std::time::Duration;
 use std::time::Instant;
 
 enum FrameSchedulerSignal {
-    NothingScheduled,
+    /// New earlier frame scheduled
     ScheduleFrameAt(Instant),
+
+    /// Exit the frame scheduler thread
     Exit,
 }
 
@@ -24,66 +28,50 @@ enum FrameSchedulerSignal {
 pub(crate) struct FrameScheduler {
     #[allow(dead_code)]
     thread: Option<std::thread::JoinHandle<()>>,
-    next_frame_time: Arc<Mutex<FrameSchedulerSignal>>,
-    frame_time_changed: Arc<Condvar>,
+    current_deadline: Arc<Mutex<Option<Instant>>>,
     fps_target: Arc<Mutex<f32>>,
+    sender: Sender<FrameSchedulerSignal>,
 }
 
 impl FrameScheduler {
     pub fn new(emit_frame: impl Fn() + Send + Sync + 'static) -> Self {
-        let next_frame_time = Arc::new(Mutex::new(FrameSchedulerSignal::NothingScheduled));
-        let frame_time_changed = Arc::new(Condvar::new());
-        let next_frame_time_thread = next_frame_time.clone();
-        let frame_time_changed_thread = frame_time_changed.clone();
-        let emit_frame = Arc::new(emit_frame);
-
+        let (tx, rx) = channel::<FrameSchedulerSignal>();
+        let current_deadline = Arc::new(Mutex::new(None));
+        let current_deadline_thrd = current_deadline.clone();
         FrameScheduler {
             thread: Some(std::thread::spawn(move || {
+                let mut now;
+                let mut next_deadline: Option<Instant>;
+                let mut wait_time: Duration;
                 loop {
-                    let mut next = next_frame_time_thread.lock().unwrap();
-
-                    // Wait for a next signal
-                    next = frame_time_changed_thread
-                        .wait_while(next, |next| {
-                            matches!(next, FrameSchedulerSignal::NothingScheduled)
-                        })
-                        .unwrap();
-
-                    let now = Instant::now();
-                    let deadline = match &*next {
-                        FrameSchedulerSignal::Exit => {
-                            break;
-                        }
-                        FrameSchedulerSignal::ScheduleFrameAt(deadline) => {
-                            if &now >= deadline {
-                                // Time to emit a frame
-                                *next = FrameSchedulerSignal::NothingScheduled;
-                                drop(next);
-                                emit_frame();
-                                continue;
-                            }
-                            *deadline
-                        }
-                        FrameSchedulerSignal::NothingScheduled => {
-                            // Should not happen due to wait_while above
-                            continue;
-                        }
+                    now = Instant::now();
+                    next_deadline = *current_deadline_thrd.lock().unwrap();
+                    wait_time = match next_deadline {
+                        Some(t) => t - now,
+                        None => Duration::MAX,
                     };
 
-                    // Sleep with timeout until deadline, but wake if notified of new signal
-                    let timeout = deadline - now;
-                    let _ = frame_time_changed_thread
-                        .wait_timeout(next, timeout)
-                        .unwrap();
-
-                    // Either timeout expired or a new earlier frame time was
-                    // set, check on next iteration which
-                    // one it was
+                    match rx.recv_timeout(wait_time) {
+                        Ok(FrameSchedulerSignal::Exit) => {
+                            break;
+                        }
+                        Ok(FrameSchedulerSignal::ScheduleFrameAt(new_deadline)) => {
+                            current_deadline_thrd.lock().unwrap().replace(new_deadline);
+                        }
+                        Err(RecvTimeoutError::Timeout) => {
+                            current_deadline_thrd.lock().unwrap().take();
+                            emit_frame();
+                        }
+                        Err(_) => {
+                            // Channel closed
+                            break;
+                        }
+                    }
                 }
             })),
             fps_target: Arc::new(Mutex::new(60.0)),
-            next_frame_time,
-            frame_time_changed,
+            current_deadline,
+            sender: tx,
         }
     }
 
@@ -96,69 +84,50 @@ impl FrameScheduler {
         *fps_target = fps.abs().max(0.0001);
     }
 
+    /// Create scheduler function
     pub fn create_scheduler(&self) -> impl Fn(Duration) + Send + Sync + 'static {
-        let next_frame_time = self.next_frame_time.clone();
-        let frame_time_changed = self.frame_time_changed.clone();
         let fps_target = self.fps_target.clone();
+        let current_deadline = self.current_deadline.clone();
+        let sender = self.sender.clone();
         move |delay: Duration| {
-            Self::schedule_frame_at(&fps_target, &next_frame_time, &frame_time_changed, delay);
+            Self::schedule_frame_at(&fps_target, &current_deadline, &sender, delay)
         }
     }
 
     /// Internal method to schedule a frame at a specific duration from now.
     fn schedule_frame_at(
         fps_target: &Arc<Mutex<f32>>,
-        next_frame_time: &Arc<Mutex<FrameSchedulerSignal>>,
-        frame_time_changed: &Arc<Condvar>,
+        current_deadline: &Arc<Mutex<Option<Instant>>>,
+        sender: &Sender<FrameSchedulerSignal>,
         delay: Duration,
     ) {
         let min_delay = Duration::from_secs_f32(1.0 / *fps_target.lock().unwrap());
         // let min_delay = std::time::Duration::from_secs(1); // ~1 FPS
         // let min_delay = std::time::Duration::from_nanos(16_666_666); // ~60 FPS
         // let min_delay = std::time::Duration::from_nanos(3_333_333); // ~300 FPS
-        let delay = if delay < min_delay { min_delay } else { delay };
-        let deadline = Instant::now() + delay;
-        let mut next = next_frame_time.lock().unwrap();
-        let new_deadline: Option<Instant> = match *next {
-            FrameSchedulerSignal::NothingScheduled => Some(deadline),
-            FrameSchedulerSignal::ScheduleFrameAt(prev) => {
-                if deadline < prev {
-                    Some(deadline)
-                } else {
-                    None
-                }
+        let delay = delay.max(min_delay);
+        let new_deadline = Instant::now() + delay;
+        if let Some(current_deadline) = *current_deadline.lock().unwrap() {
+            if new_deadline >= current_deadline {
+                return;
             }
-            FrameSchedulerSignal::Exit => None,
-        };
-
-        if let Some(new_deadline) = new_deadline {
-            *next = FrameSchedulerSignal::ScheduleFrameAt(new_deadline);
         }
 
-        drop(next);
-        if new_deadline.is_some() {
-            frame_time_changed.notify_all();
-        }
+        sender
+            .send(FrameSchedulerSignal::ScheduleFrameAt(new_deadline))
+            .unwrap();
     }
 
     /// Schedule a frame update at the specified duration from now.
     #[allow(dead_code)]
     pub fn schedule_frame(&mut self, at: Duration) {
-        Self::schedule_frame_at(
-            &self.fps_target,
-            &self.next_frame_time,
-            &self.frame_time_changed,
-            at,
-        );
+        Self::schedule_frame_at(&self.fps_target, &self.current_deadline, &self.sender, at);
     }
 }
 
 impl Drop for FrameScheduler {
     fn drop(&mut self) {
-        let mut next = self.next_frame_time.lock().unwrap();
-        *next = FrameSchedulerSignal::Exit;
-        drop(next);
-        self.frame_time_changed.notify_all();
+        let _ = self.sender.send(FrameSchedulerSignal::Exit);
         self.thread.take().unwrap().join().unwrap();
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -2,15 +2,12 @@ mod application;
 // mod egui;
 mod egui;
 mod frame_scheduler;
-mod frame_scheduler2;
 mod kind;
 mod single_color;
 
 pub use application::*;
 // pub use egui::*;
 pub use egui::*;
-#[allow(unused)]
 pub(crate) use frame_scheduler::*;
-pub(crate) use frame_scheduler2::*;
 pub use kind::*;
 pub use single_color::*;
